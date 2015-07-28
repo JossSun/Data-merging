cb9a35fb9844a930a32d02391cc1b54b12045802
JossSun <pkusunran@sina.com>
2015-07-28 16:56:13 +0800

    Update difference.py

--- difference.py
+++ difference.py
@@ -1,9 +1,31 @@
-bold = []
-f = open("rlvncy.csv","r")
-rf = csv.reader(f)
-for line in rf :
-        bold.append(line[0])
-df = open("delet-B.csv","w")
-for element in set(b).difference(set(bold)) :
-        df.write(element +'\r\n')
-        print element
+import csv
+# find the difference of A1 and QC1
+a = []
+b = []
+f1 = open("A1.csv","r")
+rf1 = csv.reader(f1)
+for line in rf1 :
+        a.append(line[0])
+        print line[0]
+        print "AAAAAAAAAA"
+
+f2 = open("QC1.csv","r")
+rf2 = csv.reader(f2)
+for line2 in rf2 :
+        b.append(line2[0])
+        print line2[0]
+        print "BBBBBBBBBB"
+print "This is list a"
+print a
+
+print "This is list b"
+print b
+df = open("delet-A1.csv","w")
+for i in a:
+        if i not in b :
+                df.write(i+'\r\n')
+                print i
+                print "FFFFFFFFFFFFFFF"
+f1.close()
+f2.close()
+df.close()
