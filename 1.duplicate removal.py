380740eb2858a939d11efe82097b457ca9759448
JossSun <pkusunran@sina.com>
2015-07-30 12:23:36 +0800

    Update 1.duplicate removal.py
    use set to remove duplications

--- 1.duplicate removal.py
+++ 1.duplicate removal.py
@@ -6,27 +6,23 @@ f1 = open("rlvncy.csv","r")
 f2 = open("Q1.csv","w")
 line = f1.readline()
 
-a = []
+a =set()
 
 iter = 0
 num  = 0
   
-while (num<24) :
-                if iter == 6 :
-                        a = list(set(a))
-                        print "The Length of query list is : %d" %len(a)
-                        iter = 0
-                if line :
+while (num<200000000) :
+        if line :
+                line.split(',')
+                query =','.join(line.split(',')[:-2])
+                a.add(query)
+                print"Updated query list count : %d" %len(a)
+                line = f1.readline()
+        else :
+                break
+        num +=1
+        print "Read in lines : %d" %num
 
-                        line.split(',')
-                        query =','.join(line.split(',')[:-2])
-                        a.append(query)
-                        print query
-                        line = f1.readline()
-                else :
-                        break
-                num +=1
-                iter +=1
 for val in a :
         f2.write(val+'\r\n')
 
