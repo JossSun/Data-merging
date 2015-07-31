3c4aba61ef32c568bb3593968bbcd8d1887d5b00
JossSun <pkusunran@sina.com>
2015-07-31 11:14:48 +0800

    Update 2.common.py

--- 2.common.py
+++ 2.common.py
@@ -1,17 +1,22 @@
+import csv
 
-b = []
-f1 = open("qdis.csv","r")
+a =set([])
+b = set([])
+
+f1 = open("Q1.csv","r")
 r1 = csv.reader(f1)
-f2 = open("qdis2.csv","r")
+f2 = open("Q2.csv","r")
 r2 = csv.reader(f2)
 f3 = open("qincommon.csv","w")
 
 for line1 in r1 :
-        a.append(line1[0])
+        a.add(line1[0])
+        print "Set A is adding"
 for line2 in r2 :
-        b.append(line2[0])
-
-tmp  = [val for val in a if val in b]
+        b.add(line2[0])
+        print "Set B is adding"
+tmp  = a&b
+print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     Finished!!!    $$$$$$$$$$$$$$$$$$$$$$$$"
 for ele in tmp :
         f3.write(ele+'\r\n')
         print ele
